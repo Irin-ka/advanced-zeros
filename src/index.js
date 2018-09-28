d28a4681097c5fa92b28dafe99de94a69e39dec6
ira <irin-ka@inbox.ru>
2018-09-28 11:31:20 +0300

    adv-zer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    var y =1000000000000000000;
+    for(var i=2;i<=base;i++){
+      if(base%i==0){
+        var c1 = 0;
+        while(base%i==0){
+          c1++;
+          base=Math.floor(base/i);
+        }
+        var c2 = 0;
+        var t = number;
+        while(t/i>0){
+          c2+=Math.floor(t/i);
+          t=Math.floor(t/i);
+        }
+        y = Math.min(y, Math.floor(c2/c1));
+      }
+    }
+    console.log(y);
+    return y;
 }
